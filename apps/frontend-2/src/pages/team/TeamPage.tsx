096bd0a540235c63e890487bbe3ee3debf6d9108
danishnav <54104284+danishnav@users.noreply.github.com>
2023-03-13 14:50:57 +0100

    Bugfix- Medlemmer lagring

--- apps/frontend-2/src/pages/team/TeamPage.tsx
+++ apps/frontend-2/src/pages/team/TeamPage.tsx
@@ -26,7 +26,7 @@ import ModalContactTeam from "../../components/team/ModalContactTeam";
 import ModalMembers from "../../components/team/ModalMembers";
 import ModalTeam from "../../components/team/ModalTeam";
 import ShortSummarySection from "../../components/team/ShortSummarySection";
-import type { ContactAddress, MemberFormValues, ProductTeamSubmitValues, Resource } from "../../constants";
+import type { ContactAddress, MemberFormValues, OfficeHoursFormValues, ProductTeamSubmitValues, Resource } from "../../constants";
 import { AddressType } from "../../constants";
 import { ResourceType } from "../../constants";
 import { Group, userHasGroup, userIsMemberOfTeam, useUser } from "../../hooks";
@@ -110,12 +110,20 @@ const TeamPage = () => {
   };
 
   const handleMemberSubmit = async (values: MemberFormValues[]) => {
-    console.log("handleSubmit2 kjøres - handleSubmit", values);
-
-    console.log({ ...mapProductTeamToFormValue(team), members: values }, "VALUES handlesubmit2");
+    let officeHoursFormatted
 
     if (team) {
-      const editResponse = await editTeam({ ...team, members: values });
+      if (team.officeHours) {
+        officeHoursFormatted = {
+          locationCode: team.officeHours.location.code,
+          days: team.officeHours.days,
+          information: team.officeHours.information
+        }
+      }
+    }
+    
+    if (team) {
+      const editResponse = await editTeam({ ...team, members: values, officeHours: officeHoursFormatted });
       await teamQuery.refetch();
       await productAreaQuery.refetch();
       await processesQuery.refetch();
